69cc9a4f3b5a3405f7d24dc707e106d8a544996e
Renishya Jayan <76423274+renishya7@users.noreply.github.com>
2022-11-05 17:45:29 +0100

    First edits

---
+++ portfolio/src/pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About" />
+        <InterestsSection sectionId="details" heading="Skills" />
+        <ProjectsSection sectionId="features" heading="Built-in Features" />
+
+      </Page>
+    </>
+  );
+}
